d316bb4511a6d1681db396f51dee97f92f9eef32
308 <sdh190304@sdh.hs.kr>
2021-04-02 17:30:23 +0900

    controller 추가

---
+++ src/main/java/com/spring/board/controller/BoardController.java
@@ -0,0 +1,14 @@
+package com.spring.board.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+
+    @RequestMapping("/index")
+    public String main() {
+        return "index";
+    }
+}
--- src/main/java/com/spring/board/controller/Controller.java
+++
@@ -1,4 +0,0 @@
-package com.spring.board.controller;
-
-public class Controller {
-}

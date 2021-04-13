c280c2066f8ba7f27eb64e4238e85c9e4b28ae24
302 <sdh190304@sdh.hs.kr>
2021-04-13 19:28:42 +0900

    DTO, Mapper추가

---
+++ src/main/java/com/spring/board/mapper/BoardMapper.java
@@ -0,0 +1,24 @@
+package com.spring.board.mapper;
+
+import com.spring.board.vo.BoardDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface BoardMapper {
+
+    public int insertBoard(BoardDTO params);
+
+    public BoardDTO selectBoardDertail(long idx);
+
+    public int updateBoard(BoardDTO params);
+
+    public int deleteBoard(long idx);
+
+    public List selectBoardList();
+
+    public int selectBoardTotalCount();
+}
--- src/main/java/com/spring/board/vo/BoardDTO.java
+++ src/main/java/com/spring/board/vo/BoardDTO.java
@@ -1,5 +1,14 @@
 package com.spring.board.vo;
 
+import lombok.*;
+
+@Getter
+@Setter
 public class BoardDTO {
 
+    private int idx;
+    private String bdTitle;
+    private String bdContent;
+    private String bdWriter;
+    private String dbDate;
 }
